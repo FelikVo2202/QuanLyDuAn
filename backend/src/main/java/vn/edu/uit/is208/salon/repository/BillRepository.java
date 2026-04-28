ee71e6bfbf000de6668696557d6eb9b017d11d03
taimv2405 <taimv2405@gmail.com>
2026-04-28 15:54:24 +0700

    perf(bill): add entityGraph to optimize findById query

--- backend/src/main/java/vn/edu/uit/is208/salon/repository/BillRepository.java
+++ backend/src/main/java/vn/edu/uit/is208/salon/repository/BillRepository.java
@@ -1,9 +1,15 @@
 package vn.edu.uit.is208.salon.repository;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import vn.edu.uit.is208.salon.entity.Bill;
 
+import java.util.Optional;
+
 public interface BillRepository extends JpaRepository<Bill, Long>, JpaSpecificationExecutor<Bill> {
     boolean existsByAppointment_Id(Long appointmentId);
+
+    @EntityGraph(attributePaths = {"details"})
+    Optional<Bill> findById(Long id);
 }
